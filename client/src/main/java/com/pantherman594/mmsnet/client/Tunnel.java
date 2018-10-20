cbf9020c7a39a067cb3de0772b1f2d50140f82ed
Benjamin Li <pharmashawarma@gmail.com>
2018-10-20 12:55:51 -0400

    Added Tunnel
    Added the tunnel class

---
+++ client/src/main/java/com/pantherman594/mmsnet/client/Tunnel.java
@@ -0,0 +1,17 @@
+package com.pantherman594.mmsnet.client;
+
+public class Tunnel {
+    String phoneNumber;
+
+    public Tunnel(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void read() {
+
+    }
+
+    public String write() {
+
+    }
+}
